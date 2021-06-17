45409281bdfe67d104d5827fc2f85285335b79fa
Jorge Tammela <alemmat@gmail.com>
2021-06-17 11:17:10 -0300

    finishing register activity

--- app/src/main/java/com/example/trackapp/MasterActivity.kt
+++ app/src/main/java/com/example/trackapp/MasterActivity.kt
@@ -1,12 +1,39 @@
 package com.example.trackapp
 
 import android.content.Intent
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.test.ApiService
+import com.example.test.Track
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 open class MasterActivity: AppCompatActivity() {
+
+
+    fun registerUser(service: ApiService, register:Register):Token{
+
+        var returnToken: Token? = Token("null","null")
+
+        service.register(register).enqueue(object: Callback<Token>{
+            override fun onResponse(call: Call<Token>?, response: Response<Token>?) {
+
+                returnToken = response?.body()!!
+
+                Log.i("retrofit register", Gson().toJson(returnToken))
+            }
+            override fun onFailure(call: Call<Token>?, t: Throwable?) {
+                t?.printStackTrace()
+            }
+        })
+
+        return returnToken!!
+    }
+
 
     fun settingRetrofit(): ApiService {
 
--- app/src/main/java/com/example/trackapp/RegisterActivity.kt
+++ app/src/main/java/com/example/trackapp/RegisterActivity.kt
@@ -1,13 +1,48 @@
 package com.example.trackapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.test.ApiService
 
 class RegisterActivity : MasterActivity() {
 
+    lateinit var service: ApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        service = this.settingRetrofit()
+
+        val btnRegister = findViewById(R.id.btnRegister) as Button
+
+        btnRegister.setOnClickListener {
+
+            val inputName = findViewById<EditText>(R.id.inputName)
+            val inputEmail = findViewById<EditText>(R.id.inputEmail)
+            val inputPassword = findViewById<EditText>(R.id.inputPassword)
+            val inputPasswordConfirm = findViewById<EditText>(R.id.inputPasswordConfirm)
+
+            val password = inputPassword.text.toString()
+            val passwordConfirm = inputPasswordConfirm.text.toString()
+
+            if ( password.equals(passwordConfirm) ){
+
+                val register = Register( inputName.text.toString(), inputEmail.text.toString(), password, passwordConfirm)
+
+                this.registerUser(service, register)
+
+            }else{
+
+                Toast.makeText(this@RegisterActivity,"Password mismatch", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
